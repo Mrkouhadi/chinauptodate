21e89ded1001c5f035e289b44aafbc29bebe1059
mrkouhadi <teacherbryan@outlook.com>
2023-08-24 01:45:48 +0800

    added model of news article

---
+++ internal/models/models.go
@@ -0,0 +1,13 @@
+package models
+
+// type Article struct {
+// 	news_id        int
+// 	category_id    int
+// 	author_id      int
+// 	news_title     string
+// 	news_image     string
+// 	news_content   string
+// 	comment_status bool
+// 	created_at     time.Time
+// 	updated_at     time.Time
+// }
