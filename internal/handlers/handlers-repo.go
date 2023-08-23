a51be6544ba53b7d300f4c4fda72c018306320ca
mrkouhadi <teacherbryan@outlook.com>
2023-08-24 01:45:00 +0800

    feat: added db to the handlers repo

--- internal/handlers/handlers-repo.go
+++ internal/handlers/handlers-repo.go
@@ -1,20 +1,25 @@
 package handlers
 
-import "github.com/mrkouhadi/chinauptodate/internal/config"
+import (
+	"github.com/mrkouhadi/chinauptodate/internal/config"
+	"github.com/mrkouhadi/chinauptodate/internal/db"
+)
 
 // repository type
 
 type Repository struct {
 	App *config.AppConfig
+	DB  db.PgxRepository // TODO:
 }
 
 // the repo that handlers methods belong to
 var Repo *Repository
 
 // NewRepo creates the new repository
-func NewRepo(a *config.AppConfig) *Repository {
+func NewRepo(a *config.AppConfig, db *db.PgxRepository) *Repository {
 	return &Repository{
 		App: a,
+		DB:  *db, // TODO:
 	}
 }
 
