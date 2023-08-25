4f6a87d5f05944bc70974e67fb6ba4b6c28d50dc
mrkouhadi <teacherbryan@outlook.com>
2023-08-26 03:03:09 +0800

    feat: added getAllArticles function

--- internal/db/postgres/get-all.go
+++ internal/db/postgres/get-all.go
@@ -1,1 +1,29 @@
 package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/mrkouhadi/chinauptodate/internal/models"
+)
+
+// ////////////////////////////////// get all articles
+func (r *PgxRepository) AllArticles() ([]models.Article, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	rows, err := r.db.Query(ctx, "SELECT * FROM news")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var allArticles []models.Article
+
+	for rows.Next() {
+		var article models.Article
+		if err := rows.Scan(&article.News_id, &article.Category_id, &article.Author_id, &article.News_title, &article.News_image, &article.News_content, &article.Comment_status, &article.Created_at, &article.Updated_at); err != nil {
+			return nil, err
+		}
+		allArticles = append(allArticles, article)
+	}
+	return allArticles, nil
+}
