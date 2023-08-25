21eeec15abbd12b1b6b3033189e7691457d27110
mrkouhadi <teacherbryan@outlook.com>
2023-08-26 03:04:25 +0800

    feat: tested update and get all articles functions

--- internal/handlers/article.go
+++ internal/handlers/article.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
@@ -11,18 +12,30 @@ import (
 )
 
 func (handlerRepo *Repository) ArticleGet(w http.ResponseWriter, r *http.Request) {
-	stringMap := make(map[string]string)
 	category := chi.URLParam(r, "category")
-	_ = chi.URLParam(r, "id") // id of the article
+	stringMap := make(map[string]string)
 	stringMap["title"] = category
-	//
-	id := uuid.MustParse("523673b4-8ca0-402f-a5fe-d71e8d485973")
-	art, err := handlerRepo.DB.GetArticleByID(id)
+	id := chi.URLParam(r, "id") // id of the article
+	id_uuid, _ := uuid.Parse(id)
+
+	// get all articles
+	articles, _ := handlerRepo.DB.AllArticles()
+	fmt.Println("Original title of the 1st article: ", articles[0].News_title)
+	//  get a single article
+	art, _ := handlerRepo.DB.GetArticleByID(id_uuid)
+	/// update article
+	newArticle := models.Article{
+		News_title:     "UPDATED the new title of this article",
+		News_image:     art.News_image,
+		News_content:   art.News_content,
+		Comment_status: true,
+		Updated_at:     time.Now(),
+	}
+	NewArticle, err := handlerRepo.DB.UpdateArticle(id_uuid, newArticle)
 	if err != nil {
 		fmt.Println(err)
-		handlerRepo.App.Session.Put(r.Context(), "error", "Could not Insert author into the database, Try again")
 	}
-	fmt.Println("article: ", art)
+	fmt.Println("The new pdated article title:", NewArticle.News_title)
 	// render html template
 	render.Template(w, r, "article.page.gohtml", &models.TemplateData{
 		StringMap: stringMap,
